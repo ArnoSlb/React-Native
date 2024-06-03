82fbd5a531d5b837f958a7e821fc0224992a04e8
ArnoSlb <soulabaille.arnaud@gmail.com>
2024-06-03 16:25:11 +0200

    feat(to-do-list-app): to do list map

--- app-to-do-list/app/index.tsx
+++ app-to-do-list/app/index.tsx
@@ -1,26 +1,41 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { useState } from "react";
 
 import { Header } from "@/components/Header/Header";
-
-import { s } from "./index.style";
 import { CardToDo } from "@/components/CardToDo/CardToDo";
 
-const TODO_LIST = [
-  { id: 1, title: "Sortir le chien", isCompleted: true },
-  { id: 2, title: "Aller chez le garagiste", isCompleted: false },
-  { id: 3, title: "Faire les courses", isCompleted: true },
-  { id: 4, title: "Appeler le vétérinaire", isCompleted: true },
-]
+import { s } from "./index.style";
 
 export default function Index() {
+
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "Sortir le chien", isCompleted: true },
+    { id: 2, title: "Aller chez le garagiste", isCompleted: false },
+    { id: 3, title: "Faire les courses", isCompleted: true },
+    { id: 4, title: "Appeler le vétérinaire", isCompleted: true },
+    { id: 5, title: "Aller chez le garagiste", isCompleted: false },
+    { id: 6, title: "Faire les courses", isCompleted: true },
+    { id: 7, title: "Appeler le vétérinaire", isCompleted: true },
+  ]);
+
+  function renderTodoList(){
+    return todoList.map((todo)=>(
+    <View style={s.cardItem} key={todo.id}>
+      <CardToDo todo={todo}/>
+    </View>
+    ));
+  }
+
   return (
     <>
       <SafeAreaProvider>
         <SafeAreaView style={s.app}>
           <Header/>
           <View style={s.body}>
-            <CardToDo todo={TODO_LIST[0]}/>
+            <ScrollView>
+              {renderTodoList()}
+            </ScrollView>
           </View>  
         </SafeAreaView>
       </SafeAreaProvider>
